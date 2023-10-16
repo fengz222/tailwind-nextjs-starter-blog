4f1da0d4c96dddb6160383809a59e201d7293078
fengzhang90 <3651228629@qq.com>
2023-10-16 21:09:30 +0800

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,7 @@
 const headerNavLinks = [
-  { href: '/', title: 'Photo' },
+  { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
+  { href: 'https://photo.fengrin.me', title: 'Photo' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
