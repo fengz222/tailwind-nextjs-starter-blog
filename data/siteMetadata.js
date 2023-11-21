3badff346ea36e423c798e81936734f6f96e27ac
fengzhang90 <3651228629@qq.com>
2023-11-21 09:39:38 +0800

    fix: yaml file format bug

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: '凛风',
+  title: '凛feng',
   author: 'FZ',
-  headerTitle: 'TailwindBlog',
+  headerTitle: 'Fengs Blog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'zh-cn',
   theme: 'system', // system, dark or light
