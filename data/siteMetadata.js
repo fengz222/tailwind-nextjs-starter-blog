1291cb6ad40fba03db5d2fc08c7a29cf192f641c
fengzhang90 <3651228629@qq.com>
2023-11-21 09:44:53 +0800

    Revert "fix: yaml file format bug"
    This reverts commit 3badff346ea36e423c798e81936734f6f96e27ac.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: '凛feng',
+  title: '凛风',
   author: 'FZ',
-  headerTitle: 'Fengs Blog',
+  headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'zh-cn',
   theme: 'system', // system, dark or light
